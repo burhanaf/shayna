365c3cafddabbe391931a60984e374b5f0de7dc0
Burhan Ahmad Fanani <burhanahmadf@gmail.com>
2020-09-17 11:53:49 +0800

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PatnerShayna />
+    <FooterShayna />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomenShayna from '@/components/WomenShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PatnerShayna from '@/components/PatnerShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PatnerShayna,
+    FooterShayna
+  }
+}
+</script>
